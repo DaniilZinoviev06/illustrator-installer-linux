bbb55b535e76ee85043535d0e9e166f148018f2e
Daniil Zinoviev <daniilzinoviev95@gmail.com>
2026-05-08 02:52:28 +0300

    Update download link for Illustrator archive

--- scripts/ai_install.sh
+++ scripts/ai_install.sh
@@ -10,7 +10,7 @@ launcherFunc() {
 }
 
 installAIFunc() {
-    local ARCHIVE="https://iusearchbtw.isgood.host/files/illustrator_17.tar.xz"
+    local ARCHIVE="https://twdl.isgood.host/shr/s5g4d7nijhwy/Illustrator_17.tar.xz"
     local ARCHIVE_NAME="illustrator_17.tar.xz"
     local TAR_SHA256="587406c49478f87f60ede4492d8cd6a5a9a1714d75e995932db6d685eec62b3a"
 
